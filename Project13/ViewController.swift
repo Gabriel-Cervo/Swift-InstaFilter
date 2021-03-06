7f4b5e39bc0c76bd3a17675a03710c562852e16b
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-06 17:37:27 -0300

    ADD imagePicker methods

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -6,18 +6,37 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensitySlider: UISlider!
-    @IBAction func intensityChanged(_ sender: UISlider) {
-    }
+    var currentImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "InstaFilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    //MARK: ImagePicker methods
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = image
     }
 
-
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
 }
 
