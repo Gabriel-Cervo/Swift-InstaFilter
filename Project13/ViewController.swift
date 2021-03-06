8de64aef0522611e35aa31b8003785061d055b09
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-06 17:28:44 -0300

    ADD design no storyboard e conexão com os outlets

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensitySlider: UISlider!
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
